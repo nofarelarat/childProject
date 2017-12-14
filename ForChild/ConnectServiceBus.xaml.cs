03c2a328a4d32a67588ab70cb287d44f79cfac37
sharon sha <sharon2108@gmail.com>
2017-12-14 20:08:18 +0200

    added the new files

---
+++ ForChild/ConnectServiceBus.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using Windows.Data.Json;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ForChild
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ConnectServiceBus : Page
+    {
+        public ConnectServiceBus()
+        {
+            this.InitializeComponent();
+        }
+
+        public async Task<user> GetUserByMailAsync(string email)
+        {
+            string completeUri = "http://localhost:49875/api/users?email=" + email;
+            Uri requestUri = new Uri(completeUri);
+
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+                user getUser = ReadToObject(httpResponseBody);
+                return getUser;
+            }
+
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+            //JsonObject js;
+            //bool ispass= JsonObject.TryParse(httpResponseBody,out js);
+            //if (ispass)
+            //{
+
+            //}
+
+            return null;
+        }
+        public static user ReadToObject(string json)
+        {
+            user deserializedUser = new user();
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(deserializedUser.GetType());
+            deserializedUser = ser.ReadObject(ms) as user;
+            //ms.Close();
+            return deserializedUser;
+        }
+
+    }
+}
