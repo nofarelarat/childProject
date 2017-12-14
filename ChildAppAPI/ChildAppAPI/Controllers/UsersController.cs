59dd171890912fd1e6ae026f86781dd85fbe4056
nofarelarat <Elarat.Nofar@gmail.com>
2017-12-14 10:11:02 -0800

    serverapi

---
+++ ChildAppAPI/ChildAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChildAppAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        public user getUser([FromUri]string email)
+        {
+            try
+            {
+                using (APP_DBEntities db = new APP_DBEntities())
+                {
+                    var user = db.users
+                    .Where(b => b.email.Equals(email))
+                    .FirstOrDefault();
+
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                return (null);
+            }
+        }
+
+        [HttpPost]
+        public bool createUser([FromBody] user user)
+        {
+            try
+            {
+                using (APP_DBEntities db = new APP_DBEntities())
+                {
+                    db.users.Add(user);
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
