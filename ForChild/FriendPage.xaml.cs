11927984e7e617c475e8270fe342e0553f57e998
noy pitterman <noypitt@gmail.com>
2017-12-22 13:42:27 +0200

    the queue is still working, and nofar code as well

--- ForChild/FriendPage.xaml.cs
+++ ForChild/FriendPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -33,12 +34,42 @@ namespace ForChild
         }
         private async void FriendSendClick(object sender, RoutedEventArgs e)
         {
-            ConnectServiceBus obj = new ConnectServiceBus();
-            int status = await obj.sendMSG("testmsg");
-            if (status != 200 & status != 204)
+            //Create an HTTP client object
+            HttpClient httpClient = new HttpClient();
+            String msg = sendMsg.Text;
+            Uri requestUri = new Uri("https://function-queue-connect.azurewebsites.net/api/HttpTriggerCSharp1-send?code=c4TP96qDiVU6X5Zd6HNmAOCOIp35R52MB0MZnL6GRjY8ldfF2GqZ3A==&&name=" + msg);
+
+            ////Send the GET request asynchronously and retrieve the response as a string.
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            try
             {
-                //ERROR MSG!!
+                ////Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                // errormessage.Text = await response.Content.ReadAsStringAsync();
             }
+            catch (Exception ex)
+            {
+
+                //  errormessage.Text = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+            /*
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            Uri requestUri = new Uri("https://function-queue-connect.azurewebsites.net/api/HttpTriggerCSharp1-send?code=c4TP96qDiVU6X5Zd6HNmAOCOIp35R52MB0MZnL6GRjY8ldfF2GqZ3A==&&name=" + "sharon);
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+            try
+            {
+                //Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }*/
         }
     }
 }
