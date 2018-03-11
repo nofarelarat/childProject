2c64566ace67945ae537d4afbe6403005bdb2ad7
nofarelarat <Elarat.Nofar@gmail.com>
2018-03-11 20:17:13 +0200

    ketboard

---
+++ ChildAppAPI/ChildAppAPI/Controllers/CountersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace ChildAppAPI.Controllers
+{
+    public class CountersController : ApiController
+    {
+        [HttpPatch]
+        public bool UpdateUserCounter([FromBody] symbol data)
+        {
+            try
+            {
+                using (APP_DBEntities db = new APP_DBEntities())
+                {
+                    db.symbols.Add(data);
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        [HttpGet]
+        public symbol[] getUserSymbolUsage([FromUri] string email, [FromUri] string symbolName)
+        {
+            try
+            {
+                using (APP_DBEntities db = new APP_DBEntities())
+                {
+                    var userSymbol = db.symbols.Where(u => u.email == email && u.symbolName == symbolName);
+
+                    if (userSymbol != null)
+                    {
+                        return userSymbol.ToArray();
+                    }
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        [HttpGet]
+        public symbol[] getUserAllSymbolsUsage([FromUri] string email)
+        {
+            try
+            {
+                using (APP_DBEntities db = new APP_DBEntities())
+                {
+                    var userSymbol = db.symbols.Where(u => u.email == email);
+
+                    if (userSymbol != null)
+                    {
+                        return userSymbol.ToArray();
+                    }
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ForChild/userContacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForChild
+{
+    public partial class userContacts
+    {
+        public string email { get; set; }
+        public string father { get; set; }
+        public string mother { get; set; }
+        public string sister { get; set; }
+        public string friend { get; set; }
+    }
+
+}
