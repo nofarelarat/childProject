ef100fc8bd34c32e99f8a463d2c327c1e3c797a2
noy pitterman <noypitt@gmail.com>
2018-03-12 21:38:44 +0200

    added out_Table.cs

---
+++ ForChild/OutTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForChild
+{
+    class OutTable : TableEntity
+    {
+        public string PartitionKey;
+        public string RowKey;
+        public string Timestamp;
+        public string TimeStamp;
+        public string Message;
+        public string Message_Send;
+        public string Message_Recive;
+
+        public OutTable() { }
+    }
+
+}
+
