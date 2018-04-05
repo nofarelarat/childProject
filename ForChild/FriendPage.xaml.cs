d0d16ec43c475c74105e8244e473ed9517417d40
sharon sha <sharon2108@gmail.com>
2018-04-05 19:14:31 +0300

    714

--- ForChild/FriendPage.xaml.cs
+++ ForChild/FriendPage.xaml.cs
@@ -16,12 +16,16 @@ namespace ForChild
         static Image[] symbolsForSend1 = new Image[5];
         static Image[] symbolsForSend2 = new Image[5];
         static Image[] symbolsForSend3 = new Image[5];
+        static Image[] symbolsForSend4 = new Image[5];
+
         //index in massege
         static int symbolsForSend_curr1 = 0;
         static int symbolsForSend_curr2 = 0;
         static int symbolsForSend_curr3 = 0;
+        static int symbolsForSend_curr4 = 0;
+
         // full/open flag
-        static int[] symbolsForSend_full = new int[3];
+        static int[] symbolsForSend_full = new int[4];
 
         static Image[] symbolsSentFromOther1 = new Image[5];
         static Image[] symbolsSentFromOther2 = new Image[5];
@@ -29,6 +33,9 @@ namespace ForChild
 
         static int[] symbolsSentFromOther_full = new int[3];
         static bool flag = true;
+        static bool iStarted = true;
+        static int my_num_of_msg = 0;
+
 
         public FriendPage()
         {
@@ -44,6 +51,8 @@ namespace ForChild
                 symbolsForSend1[i].Source = null;
                 symbolsForSend2[i].Source = null;
                 symbolsForSend3[i].Source = null;
+                symbolsForSend4[i].Source = null;
+
                 symbolsSentFromOther1[i].Source = null;
                 symbolsSentFromOther2[i].Source = null;
                 symbolsSentFromOther3[i].Source = null;
@@ -53,10 +62,14 @@ namespace ForChild
             symbolsForSend_curr1 = 0;
             symbolsForSend_curr2 = 0;
             symbolsForSend_curr3 = 0;
+            symbolsForSend_curr4 = 0;
+
 
             symbolsForSend_full[0] = 0;
             symbolsForSend_full[1] = 0;
             symbolsForSend_full[2] = 0;
+            symbolsForSend_full[3] = 0;
+
 
             symbolsSentFromOther_full[0] = 0;
             symbolsSentFromOther_full[1] = 0;
@@ -74,6 +87,8 @@ namespace ForChild
                 symbolsForSend1[i].Source = null;
                 symbolsForSend2[i].Source = null;
                 symbolsForSend3[i].Source = null;
+                symbolsForSend4[i].Source = null;
+
                 symbolsSentFromOther1[i].Source = null;
                 symbolsSentFromOther2[i].Source = null;
                 symbolsSentFromOther3[i].Source = null;
@@ -83,151 +98,179 @@ namespace ForChild
             symbolsForSend_curr1 = 0;
             symbolsForSend_curr2 = 0;
             symbolsForSend_curr3 = 0;
+            symbolsForSend_curr4 = 0;
+
 
             symbolsForSend_full[0] = 0;
             symbolsForSend_full[1] = 0;
             symbolsForSend_full[2] = 0;
+            symbolsForSend_full[3] = 0;
+
             Common.DeleteFileAsync("chatWithFriend.txt");
             flag = true;
             GetMsgFromFriend();
         }
-        private async void FriendSendClick(object sender, RoutedEventArgs e)
-        {
-            int message_num = 0;
-            string sentence = "";
-            Image[] symbolsForSend = symbolsForSend1;
-            int fullFlag = 0;
-            if (symbolsForSend_full[0] != 0)
-            {
-                if (symbolsForSend_full[1] == 0)
-                {
-                    message_num = 2;
-                    symbolsForSend_full[1] = 1;
-                }
-                else if (symbolsForSend_full[2] == 0)
-                {
-                    message_num = 3;
-                    symbolsForSend_full[2] = 1;
-                }
-                else
-                {//all full
-                    fullFlag = 1;
-                }
-            }
-            else
-            {
-                message_num = 1;
-                symbolsForSend_full[0] = 1;
-            }
-            if (fullFlag == 0)
-            {
-                for (int i = 0; i < symbolsForSend1.Length; i++)
-                {
-                    if (message_num == 1)
-                    {
-                        sentence = sentence + symbolsForSend1[i].Tag.ToString() + "+";
-                        Common.UpdateCounterAsync(symbolsForSend1[i].Tag.ToString());
-                    }
-                    else if (message_num == 2)
-                    {
-                        sentence = sentence + symbolsForSend2[i].Tag.ToString() + "+";
-                        Common.UpdateCounterAsync(symbolsForSend2[i].Tag.ToString());
-                    }
-                    else if (message_num == 3)
-                    {
-                        sentence = sentence + symbolsForSend3[i].Tag.ToString() + "+";
-                        Common.UpdateCounterAsync(symbolsForSend3[i].Tag.ToString());
-                    }
-                }
-            }
-            //To do : sent to is hard coded!!
-            Common.sendMsg(sentence, Common.myMother);
-            send.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-        }
+
+        //private async void FriendSendClick(object sender, RoutedEventArgs e)
+        //{
+        //    int message_num = 0;
+        //    string sentence = "";
+        //    Image[] symbolsForSend = symbolsForSend1;
+        //    int fullFlag = 0;
+        //    if (symbolsForSend_full[0] != 0)
+        //    {
+        //        if (symbolsForSend_full[1] == 0)
+        //        {
+        //            message_num = 2;
+        //            symbolsForSend_full[1] = 1;
+        //        }
+        //        else if (symbolsForSend_full[2] == 0)
+        //        {
+        //            message_num = 3;
+        //            symbolsForSend_full[2] = 1;
+        //        }
+        //        else
+        //        {//all full
+        //            fullFlag = 1;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        message_num = 1;
+        //        symbolsForSend_full[0] = 1;
+        //    }
+        //    if (fullFlag == 0)
+        //    {
+        //        for (int i = 0; i < symbolsForSend1.Length; i++)
+        //        {
+        //            if (message_num == 1)
+        //            {
+        //                sentence = sentence + symbolsForSend1[i].Tag.ToString() + "+";
+        //                Common.UpdateCounterAsync(symbolsForSend1[i].Tag.ToString());
+        //            }
+        //            else if (message_num == 2)
+        //            {
+        //                sentence = sentence + symbolsForSend2[i].Tag.ToString() + "+";
+        //                Common.UpdateCounterAsync(symbolsForSend2[i].Tag.ToString());
+        //            }
+        //            else if (message_num == 3)
+        //            {
+        //                sentence = sentence + symbolsForSend3[i].Tag.ToString() + "+";
+        //                Common.UpdateCounterAsync(symbolsForSend3[i].Tag.ToString());
+        //            }
+        //        }
+        //    }
+        //    //To do : sent to is hard coded!!
+        //    Common.sendMsg(sentence, Common.myMother);
+        //    send.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        //}
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
-            int message_num = 0;
+            my_num_of_msg++;
+            int index = my_num_of_msg;
             string sentence = "";
-            Image[] symbolsForSend = symbolsForSend1;
-            int fullFlag = 0;
-            if (symbolsForSend_full[0] != 0)
+            if (iStarted == false)
             {
-                if (symbolsForSend_full[1] == 0)
-                {
-                    message_num = 2;
-                    symbolsForSend_full[1] = 1;
-                }
-                else if (symbolsForSend_full[2] == 0)
-                {
-                    message_num = 3;
-                    symbolsForSend_full[2] = 1;
-                }
-                else
-                {//all full
-                    fullFlag = 1;
-                }
+                index += 1;
             }
-            else
-            {
-                message_num = 1;
-                symbolsForSend_full[0] = 1;
-            }
-            if (fullFlag == 0)
+            symbolsForSend_full[index-1] = 1;
+
+            //int message_num = 0;
+            //string sentence = "";
+            //Image[] symbolsForSend = symbolsForSend1;
+            //if (symbolsForSend_full[0] != 0)
+            //{
+            //    if (symbolsForSend_full[1] == 0)
+            //    {
+            //        message_num = 2;
+            //        symbolsForSend_full[1] = 1;
+            //    }
+            //    else if (symbolsForSend_full[2] == 0)
+            //    {
+            //        message_num = 3;
+            //        symbolsForSend_full[2] = 1;
+            //    }
+            //    else
+            //    {//all full
+            //        fullFlag = 1;
+            //    }
+            //}
+            //else
+            //{
+            //    message_num = 1;
+            //    symbolsForSend_full[0] = 1;
+            //}
+            if (my_num_of_msg <= 3)
             {
                 for (int i = 0; i < symbolsForSend1.Length; i++)
                 {
-                    if (message_num == 1)
+                    if (index == 1)
                     {
                         sentence = sentence + symbolsForSend1[i].Tag.ToString() + "-";
                         Common.UpdateCounterAsync(symbolsForSend1[i].Tag.ToString());
                     }
-                    else if (message_num == 2)
+                    else if (index == 2)
                     {
                         sentence = sentence + symbolsForSend2[i].Tag.ToString() + "-";
                         Common.UpdateCounterAsync(symbolsForSend2[i].Tag.ToString());
                     }
-                    else if (message_num == 3)
+                    else if (index == 3)
                     {
                         sentence = sentence + symbolsForSend3[i].Tag.ToString() + "-";
                         Common.UpdateCounterAsync(symbolsForSend3[i].Tag.ToString());
                     }
+                    else if (index == 4)
+                    {
+                        sentence = sentence + symbolsForSend4[i].Tag.ToString() + "-";
+                        Common.UpdateCounterAsync(symbolsForSend4[i].Tag.ToString());
+                    }
+
+
                 }
             }
             //To do : sent to is hard coded!!
             Common.sendMsg(sentence, Common.myFriend);
             send.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             Common.WriteConversation("child:" + sentence, "chatWithFriend.txt");
+            Common.WriteConversation("my_num_of_msg:" + my_num_of_msg, "chatWithFriend.txt");
+
         }
 
         private void Symbol_Click(object sender, RoutedEventArgs e)
         {
-            int symbolsForSend_messege = -1;
-            if (symbolsForSend_full[0] != 0)
+            int index = my_num_of_msg+1;
+            if (iStarted == false)
             {
-                if (symbolsForSend_full[1] != 0)
-                {
-                    if (symbolsForSend_full[2] == 0)
-                    {
-                        symbolsForSend_messege = 3;
-                    }
-                }
-                else
-                {
-                    symbolsForSend_messege = 2;
-                }
+                index += 1;
             }
-            else
-            {
-                symbolsForSend_messege = 1;
-            }
+
+
+            //int symbolsForSend_messege = -1;
+            //if (symbolsForSend_full[0] != 0)
+            //{
+            //    if (symbolsForSend_full[1] != 0)
+            //    {
+            //        if (symbolsForSend_full[2] == 0)
+            //        {
+            //            symbolsForSend_messege = 3;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        symbolsForSend_messege = 2;
+            //    }
+            //}
+            //else
+            //{
+            //    symbolsForSend_messege = 1;
+            //}
             Button button = (Button)sender;
             string button_name = button.Name;
-            if (symbolsForSend_messege != -1)
-            {
-                AddToForSendGrid(button_name, symbolsForSend_messege);
-            }
+            AddToForSendGrid(button_name, index);
+            
         }
+
         private void AddToForSendGrid(string button_name, int symbolsForSend_curr)
         {
             Uri requestUri = new Uri(base.BaseUri, "/symbols/" + button_name + ".png");
@@ -249,10 +292,21 @@ namespace ForChild
                 symbolsForSend3[symbolsForSend_curr3].Tag = button_name;
                 symbolsForSend_curr3++;
             }
+            if (symbolsForSend_curr == 4 && symbolsForSend_curr4 < 5)
+            {
+                symbolsForSend3[symbolsForSend_curr4].Source = new BitmapImage(requestUri);
+                symbolsForSend3[symbolsForSend_curr4].Tag = button_name;
+                symbolsForSend_curr4++;
+            }
         }
 
         private void GetMessageImg(Image[] symbolsSentFromOther)
         {
+            if (symbolsForSend_full[0] == 0) {
+                symbolsForSend_full[0] = 1;
+                iStarted = false;
+            }
+
             if (symbolsSentFromOther_full[0] == 0)
             {
                 for (int i = 0; i < symbolsSentFromOther1.Length; i++)
@@ -312,20 +366,22 @@ namespace ForChild
             }
         }
 
-        private async Task GetMessageAsync(OutTable[] message)
+        private async Task GetMessageAsync(OutTable[] messages)
         {
             Image[] images = new Image[5];
 
             int numofmsg = 0; //the number of messages cant be more than 3.
                               //TODO : add to the if 
-            if (message != null && message.Length > 0)
+            if (messages != null && messages.Length > 0)
             {
-                for (int x = 0; x < message.Length; x++)
+                for (int x = 0; x < messages.Length; x++)
                 {
                     int i = 0;
                     numofmsg++;
-                    string msg = message[x].Message;
+                    string msg = messages[x].Message;
                     await Common.WriteConversation("friend:" + msg, "chatWithFriend.txt");
+                    await Common.WriteConversation("iStarted:" + iStarted, "chatWithFriend.txt");
+
                     string[] tmp = msg.Split('-');
                     foreach (string source in tmp)
                     {
@@ -342,9 +398,9 @@ namespace ForChild
                         return;
                     }//if
                 }
-                for (int x = 0; x < message.Length; x++)
+                for (int x = 0; x < messages.Length; x++)
                 {
-                    await Common.markAsDeleteMsg(message[x]);
+                    await Common.markAsDeleteMsg(messages[x]);
                 }
             }
         }
@@ -379,6 +435,12 @@ namespace ForChild
             symbolsForSend3[3] = forSend34;
             symbolsForSend3[4] = forSend35;
 
+            symbolsForSend4[0] = forSend41;
+            symbolsForSend4[1] = forSend42;
+            symbolsForSend4[2] = forSend43;
+            symbolsForSend4[3] = forSend44;
+            symbolsForSend4[4] = forSend45;
+
             symbolsSentFromOther1[0] = afterSend11;
             symbolsSentFromOther1[1] = afterSend12;
             symbolsSentFromOther1[2] = afterSend13;
@@ -409,33 +471,47 @@ namespace ForChild
                 for (int i = 0; i < messages.Length; i++)
                 {
                     string[] message = messages[i].Split(':', '-');
-                    Image[] images = new Image[5];
-
-                    for (int j = 0; j < images.Length; j++)
+                    if (message[0].Equals("iStarted"))
                     {
-                        Uri requestUri = new Uri(base.BaseUri, "/symbols/" + ".png");
-
-                        if (j + 1 < message.Length)
-                        {
-                            requestUri = new Uri(base.BaseUri, "/symbols/" + message[j + 1] + ".png");
+                        if (message[1].Equals("true")) {
+                            iStarted = true;
                         }
-                        images[j] = new Image();
-                        images[j].Source = new BitmapImage(requestUri);
+                        iStarted = false;
+                    }
+                    else if (message[0].Equals("my_num_of_msg"))
+                    {
+                        my_num_of_msg = Int32.Parse(message[1]);
+                    }
+                    else {
+                        Image[] images = new Image[5];
 
-                    }
-                    if (message[0].Equals("friend"))
-                    {
-                        GetMessageImg(images);
-                    }
-                    if (message[0].Equals("child"))
-                    {
-                        GetSentMessage(images);
+                        for (int j = 0; j < images.Length; j++)
+                        {
+                            Uri requestUri = new Uri(base.BaseUri, "/symbols/" + ".png");
+
+                            if (j + 1 < message.Length)
+                            {
+                                requestUri = new Uri(base.BaseUri, "/symbols/" + message[j + 1] + ".png");
+                            }
+                            images[j] = new Image();
+                            images[j].Source = new BitmapImage(requestUri);
+
+                        }
+                        if (message[0].Equals("friend"))
+                        {
+                            GetMessageImg(images);
+                        }
+                        if (message[0].Equals("child"))
+                        {
+                            GetSentMessage(images);
+                        }
+
+                        if (symbolsForSend_full.Sum() > symbolsSentFromOther_full.Sum())
+                        {
+                            send.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                        }
                     }
 
-                    if (symbolsForSend_full.Sum() > symbolsSentFromOther_full.Sum())
-                    {
-                        send.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-                    }
                 }
             }
         }
